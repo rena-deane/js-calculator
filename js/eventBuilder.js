9d466b4110933f5e8edb1e4d820a64fa312e43c2
Rena Goldsmith <rena.kg@gmail.com>
2016-05-06 14:15:32 +1200

    add click listeners to butn 7-0

--- js/eventBuilder.js
+++ js/eventBuilder.js
@@ -1,7 +1,25 @@
-function addNum(){
 
-}
+
+// seven
+var getSeven = document.getElementById('seven');
+getSeven.addEventListener("click", function(){
+    document.getElementById('solutionID').value += 7;
+  });
+
+// eight
+var getEight = document.getElementById('eight');
+getEight.addEventListener("click", function(){
+    document.getElementById('solutionID').value += 8;
+  });
 
 // nine
 var getNine = document.getElementById('nine');
-getNine.addEventListener("click", addNum());
+getNine.addEventListener("click", function(){
+    document.getElementById('solutionID').value += 9;
+  });
+
+// zero
+var getZero = document.getElementById('zero');
+getZero.addEventListener("click", function(){
+    document.getElementById('solutionID').value += 0;
+  });
